e8ccd3f3dae09f84ae27d633776d9fcef1a0031b
Gagskaya <alisxan006@mail.ru>
2021-03-24 16:11:26 +0300

    done

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import Anime from 'react-anime';
-import '../styles/AboutCard.css';
+import React from "react";
+import Anime from "react-anime";
+import "../styles/AboutCard.css";
 
 function AboutCard(props) {
-  return(
-    <Anime easing="easeInSine" opacity={[0, 1]} scale={props.scaleArr} delay={100} duration={300}>
-      <div className="card about-card" onClick={props.onClick('about')} onMouseEnter={props.MouseEnter('about')} onMouseLeave={props.MouseLeave('about')}>
-        <h2>Гапур</h2>
+  return (
+    <Anime
+      easing="easeInSine"
+      opacity={[0, 1]}
+      scale={props.scaleArr}
+      delay={100}
+      duration={300}
+    >
+      <div
+        className="card about-card"
+        onClick={props.onClick("about")}
+        onMouseEnter={props.MouseEnter("about")}
+        onMouseLeave={props.MouseLeave("about")}
+      >
+        <h2>Илисхан</h2>
         <p>Front-end разработчик.</p>
       </div>
     </Anime>
-  )
+  );
 }
 
 export default AboutCard;
